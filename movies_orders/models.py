2aba3f118621f5b08fc5c00dcab46c2f697e2622
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-10-03 18:19:26 -0300

    feat: create movie orders model and make migrations

---
+++ movies_orders/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class MovieOrder(models.Model):
+    purchased_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    movie = models.ForeignKey("movies.Movie", on_delete=models.CASCADE)
+
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
