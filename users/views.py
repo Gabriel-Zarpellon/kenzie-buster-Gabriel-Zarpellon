1c8d99b76af55a84cfed627c261f72e5b18621e8
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-10-07 13:23:59 -0300

    feat: create views for get and patch user by id

--- users/views.py
+++ users/views.py
@@ -4,6 +4,11 @@ from users.serializers import UserSerializer
 from .serializers import CustomJWTSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from .permissions import IsUserAllowed
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import User
+
 
 class UserView(APIView):
     def post(self, request: Request) -> Response:
@@ -13,6 +18,39 @@ class UserView(APIView):
         serializer.save()
 
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+class UserDetailedView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsUserAllowed]
+
+    def get(self, request: Request, user_id: int) -> Response:
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return Response({"detail": "Not found."}, status.HTTP_404_NOT_FOUND)
+
+        self.check_object_permissions(request, user)
+
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def patch(self, request: Request, user_id: int) -> Response:
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return Response({"detail": "Not found."}, status.HTTP_404_NOT_FOUND)
+
+        self.check_object_permissions(request, user)
+
+        serializer = UserSerializer(user, data=request.data, partial=True)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_200_OK)
 
 
 class LoginJWTView(TokenObtainPairView):
