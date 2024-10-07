b98e71eecc5aa9ff5b056bfdff6ad8cce161e14a
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-10-07 13:24:21 -0300

    feat: add user detailed view path

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserView, LoginJWTView
+from .views import UserView, LoginJWTView, UserDetailedView
 
 urlpatterns = [
     path("users/", UserView.as_view()),
     path("users/login/", LoginJWTView.as_view()),
+    path("users/<int:user_id>/", UserDetailedView.as_view()),
 ]
